7d3a47d84a04f718a702ce21f650feece4ea0e15
lsq <lisingqing@inspur.com>
2021-04-27 08:46:41 +0800

    origin commit on 2021-4-27

--- scripts/src/5.7.28/GET_SOURCE.links.sh
+++ scripts/src/5.7.28/GET_SOURCE.links.sh
@@ -1,7 +1,7 @@
 ##1.TAR source
-wget -c https://downloads.mysql.com/archives/get/p/23/file/mysql-boost-5.7.31.tar.gz
+wget -c https://downloads.mysql.com/archives/get/p/23/file/mysql-boost-5.7.28.tar.gz
 ##2. RPM Source 
-wget -c https://downloads.mysql.com/archives/get/p/23/file/mysql-community-5.7.31-1.el7.src.rpm
+wget -c https://downloads.mysql.com/archives/get/p/23/file/mysql-community-5.7.28-1.el7.src.rpm
 
 
 
